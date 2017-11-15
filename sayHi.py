53d77406d29c477f8f3c42d4647c49db91d191b2
ne_andrey <sorokinac@gmail.com>
2017-11-15 19:46:17 +0500

    add_sayHi_file

---
+++ sayHi.py
@@ -0,0 +1,13 @@
+def say_hi(name, age):
+    """
+        Hi!
+    """
+    strAge = str(age)
+    strConvert = ("Hi. My name is %s and I'm %s years old" % (name, strAge))
+    return strConvert
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert say_hi("Alex", 32) == "Hi. My name is Alex and I'm 32 years old", "First"
+    assert say_hi("Frank", 68) == "Hi. My name is Frank and I'm 68 years old", "Second"
+    print('Done. Time to Check.')
