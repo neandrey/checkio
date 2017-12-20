483a615f27ebac0f2ed086192a6657d28ba2e353
ne_andrey <sorokinac@gmail.com>
2017-12-21 02:25:52 +0500

    add_stripedWord_file. function whiteSymbol Error_convert_letter_in[' ',' '].

--- stripedWord.py
+++ stripedWord.py
@@ -1,66 +1,78 @@
 VOWELS = "AEIOUY"
 CONSONANTS = "BCDFGHJKLMNPQRSTVWXZ"
-
-import re
-#---------------------
-def lowerLetter(text):
-    """
-    :param text: string simbol
-    :return: lower string simbol
-    """
-    return text.lower()
 #--------------------------
-def removeSingleAndCountWord(text):
-    '''
+import re
+notWhite = re.compile('\W')
+
+#---------------------
+def highFunc(text):
+    """
     :param text: string simbol
-    :return: L->[], world - count word
-    '''
-    world = 0
-    L = []
-    for tx in text:
-        z = text.index(tx)
-        if tx == ' ' and re.match(r'[a-z]', text[z+1]):
-            world += 1
-        if re.match(r'[a-z\s]', tx):
-            L.append(tx)
-    return L, world
-#---------------------------
-def checkFunction(L, t1, t2):
-    '''
-    :param L:   data simbol
-    :param t1:  test arraySimbol1
-    :param t2:  test arraySimbol2
-    :return:    True or False
-    '''
+    :return: high string simbol
+    """
+    return text.upper()
+#--------------------------------
+def whiteSymbol(text):
+    """
+    :param text - input string:
+    :return: str without is symbols ,./ and other '\W'.
+    """
+    Str = str()
     i = 0
-    try:
-        while (True):
-            if L[i+1] == ' ':
+    for let in text:
+        if notWhite.match(let):
+            Str += ' '
+        else:
+            Str += let
+        i += 1
 
-            if(L[i] in t1) == (L[i+1] in t1) or (L[i] in t2) == (L[i+1] in t2):
-                return False
-            i += 1
-    except TypeError:
-        print('hello')
-
-
+    return Str
 
 #--------------------------------
+def counterWord(text):
+    counterWords = 0
+
+    while text:
+        flagCounter = 0
+        temp = text[:1]
+        #---------------------------
+        for i in temp[0]:
+            if (len(temp[0]) == 1):
+                break
+            if ((i in VOWELS) and (flagCounter == -1)) or ((i in CONSONANTS) and (flagCounter == 1)):
+                 break
+            if (i in VOWELS) and (flagCounter == 1 or flagCounter == 0):
+                flagCounter = -1
+            if (i in CONSONANTS) and (flagCounter == -1 or flagCounter == 0):
+                flagCounter = 1
+        #-----------------------------
+        else:
+            counterWords += 1
+        text = text[1:]
+
+    return counterWords
+
+#------------------------------------
 def checkio(text):
-    t1 = lowerLetter(VOWELS)
-    t2 = lowerLetter(CONSONANTS)
-    text = lowerLetter(text)
-    L, world = removeSingleAndCountWord(text)
-    print(L, world)
-    if checkFunction(L, t1, t2):
-        return world
-    else:
-        return 0
+    text = highFunc(text)
+    print(text)
+    text = whiteSymbol(text).strip(' ')
+    print(text)
+    text = text.split(' ')
+    print(text)
+    counterWords = counterWord(text)
+    print(counterWords)
+
+
+    return counterWords
+
+#-----------------------------------------
 
 
 #These "asserts" using only for self-checking and not necessary for auto-testing
 if __name__ == '__main__':
-    assert checkio("My name is ...") == 3, "All words are striped"
-    assert checkio("Hello world") == 0, "No one"
-    assert checkio("A quantity of striped words.") == 1, "Only of"
-    assert checkio("Dog,cat,mouse,bird.Human.") == 3, "Dog, cat and human"
+#    assert checkio("My name is ...") == 3, "All words are striped"
+#    assert checkio("Hello world") == 0, "No one"
+#   assert checkio("A quantity of striped words.") == 1, "Only of"
+#  assert checkio("Dog,cat,mouse,bird.Human.") == 3, "Dog, cat and human"
+    assert checkio("Lorem ipsum dolor sit amet, consectetuer adipiscing elit.") == 3, "Dog, cat and human"
